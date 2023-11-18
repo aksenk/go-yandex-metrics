f1fd91715bd1f759040a8a679393d74bd39d861c
Николай Аксенов <nk_aksenov@betboom.org>
2023-11-18 19:54:02 +0300

    agent handlers tests completed

--- internal/agent/handlers/handlers_test.go
+++ internal/agent/handlers/handlers_test.go
@@ -6,13 +6,17 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 )
 
-func TestGenerateSendURL(t *testing.T) {
+// вроде как имена тестов нужно писать через CamelCase, но если это приватная функция
+// то генератор в IDE даёт ей название через snake_case. это ок?
+func Test_generateSendURL(t *testing.T) {
 	tests := []struct {
-		name   string
-		metric models.Metric
-		want   string
+		name    string
+		metric  models.Metric
+		wantErr bool
+		want    string
 	}{
 		{
 			name: "basic logic test",
@@ -21,7 +25,8 @@ func TestGenerateSendURL(t *testing.T) {
 				Type:  "counter",
 				Value: 1,
 			},
-			want: "/counter/PollInterval/1",
+			wantErr: false,
+			want:    "/counter/PollInterval/1",
 		},
 		{
 			name: "basic logic test dummy values",
@@ -30,14 +35,29 @@ func TestGenerateSendURL(t *testing.T) {
 				Type:  "akunamatata",
 				Value: "keeeks",
 			},
-			want: "/akunamatata/tratatatata/keeeks",
+			wantErr: false,
+			want:    "/akunamatata/tratatatata/keeeks",
+		},
+		{
+			name: "error test",
+			metric: models.Metric{
+				Name:  "PollInterval",
+				Type:  "counter",
+				Value: 1,
+			},
+			wantErr: true,
+			want:    "/gauge/PollInterval/1",
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			get := generateSendURL(tt.metric, "")
-			assert.Equal(t, tt.want, get)
+			if tt.wantErr {
+				assert.NotEqual(t, tt.want, get)
+			} else {
+				assert.Equal(t, tt.want, get)
+			}
 		})
 	}
 }
@@ -53,7 +73,7 @@ func Test_sendMetrics(t *testing.T) {
 		wantErr bool
 	}{
 		{
-			name:    "correct test",
+			name:    "successful test",
 			wantErr: false,
 			args: args{
 				metrics: []models.Metric{
@@ -67,7 +87,7 @@ func Test_sendMetrics(t *testing.T) {
 			},
 		},
 		{
-			name:    "incorrect test",
+			name:    "unsuccessful test",
 			wantErr: true,
 			args: args{
 				metrics: []models.Metric{
@@ -84,8 +104,6 @@ func Test_sendMetrics(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-				t.Log(tt.args.path)
-				t.Log(r.URL.RequestURI())
 				if tt.wantErr {
 					assert.NotEqual(t, tt.args.path, r.URL.RequestURI())
 
@@ -97,6 +115,85 @@ func Test_sendMetrics(t *testing.T) {
 			defer s.Close()
 			err := sendMetrics(tt.args.metrics, s.URL)
 			assert.Equal(t, nil, err)
+		})
+	}
+}
+
+func TestHandleMetrics(t *testing.T) {
+	type args struct {
+		handleAfter int
+		checkAfter  int
+		metrics     []models.Metric
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name:    "successful test",
+			wantErr: false,
+			args: args{
+				handleAfter: 1,
+				checkAfter:  2,
+				metrics: []models.Metric{
+					{
+						Name:  "FirstMetric",
+						Type:  "gauge",
+						Value: 1.123,
+					},
+					{
+						Name:  "SecondMetric",
+						Type:  "counter",
+						Value: 10,
+					},
+				},
+			},
+		},
+		{
+			name:    "unsuccessful test",
+			wantErr: true,
+			args: args{
+				handleAfter: 3,
+				checkAfter:  2,
+				metrics: []models.Metric{
+					{
+						Name:  "FirstMetric",
+						Type:  "gauge",
+						Value: 1.123,
+					},
+					{
+						Name:  "SecondMetric",
+						Type:  "counter",
+						Value: 10,
+					},
+				},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		c := make(chan []models.Metric, 1)
+		t.Run(tt.name, func(t *testing.T) {
+			s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.Write([]byte("ok"))
+			}))
+			checkAfter := time.Second * time.Duration(tt.args.checkAfter)
+			ticker := time.NewTicker(time.Second * time.Duration(tt.args.handleAfter))
+			c <- tt.args.metrics
+			go HandleMetrics(c, ticker, s.URL)
+			time.Sleep(checkAfter)
+			var data []models.Metric
+			select {
+			case data = <-c:
+				if !tt.wantErr {
+					assert.Nil(t, data, "The channel is not empty")
+				}
+			default:
+				if tt.wantErr {
+					assert.NotNil(t, data, "The channel is empty")
+				}
+			}
 		})
 	}
 }
