647cfc0ca6e62aefd5aae91c4cd107b9f868e2db
Николай Аксенов <nk_aksenov@betboom.org>
2023-11-27 01:47:59 +0300

    remove internal/server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,26 +1,16 @@
 package main
 
 import (
-	"github.com/aksenk/go-yandex-metrics/internal/models"
 	"github.com/aksenk/go-yandex-metrics/internal/server/handlers"
-	"github.com/aksenk/go-yandex-metrics/internal/server/server"
 	"github.com/aksenk/go-yandex-metrics/internal/server/storage/memstorage"
-	"log"
+	"net/http"
 )
 
 func main() {
 	listenAddr := "localhost:8080"
 	listenPath := "/update/"
-	// хз зачем делать объявление переменной тут,
-	// но если в след строчке написать напрямую storage.Storage.NewMemStorage() - это не работает
 	s := memstorage.NewMemStorage()
-	srv := models.Server{
-		ListenAddr: listenAddr,
-		ListenURL:  listenPath,
-		Handler:    handlers.UpdateMetric(s),
-	}
-	err := server.NewServer(srv)
-	if err != nil {
-		log.Fatal(err)
-	}
+	mux := http.NewServeMux()
+	mux.HandleFunc(listenPath, handlers.UpdateMetric(s))
+	http.ListenAndServe(listenAddr, mux)
 }
