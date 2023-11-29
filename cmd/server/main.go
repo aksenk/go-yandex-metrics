ff2befd9a2fd3921801c512227aad0971316b706
Николай Аксенов <nk_aksenov@betboom.org>
2023-11-30 02:29:15 +0300

    add config from envs

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,12 +6,17 @@ import (
 	"github.com/aksenk/go-yandex-metrics/internal/server/storage/memstorage"
 	"log"
 	"net/http"
+	"os"
 )
 
 // TODO просто вопрос: как запускать тесты сразу по всем директориям?
 func main() {
 	listenAddr := flag.String("a", "localhost:8080", "host:port for server listening")
 	flag.Parse()
+	if e := os.Getenv("ADDRESS"); e != "" {
+		listenAddr = &e
+	}
+
 	s := memstorage.NewMemStorage()
 	r := handlers.NewRouter(s)
 	log.Printf("Starting web server on %v", *listenAddr)
