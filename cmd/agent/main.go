03386e64e1f0d01ec403dcacdc82addffa32acad
Николай Аксенов <nk_aksenov@betboom.org>
2023-11-30 00:25:49 +0300

    added using flags

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,22 +1,36 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"github.com/aksenk/go-yandex-metrics/internal/agent/handlers"
 	"github.com/aksenk/go-yandex-metrics/internal/agent/metrics"
 	"github.com/aksenk/go-yandex-metrics/internal/models"
+	"log"
 	"time"
 )
 
 func main() {
-	serverURL := "http://localhost:8080/update"
-	pollInterval := 2
-	reportInterval := 10
+	var serverURL string
+	serverHTTPScheme := flag.Bool("s", false, "Use HTTPS connection to the server")
+	serverAddr := flag.String("a", "localhost:8080", "Metrics server address (host:port)")
+	pollInterval := flag.Int("p", 2, "Interval for scraping metrics (in seconds)")
+	reportInterval := flag.Int("r", 10, "Interval for sending metrics (in seconds)")
+	flag.Parse()
+	if *serverHTTPScheme {
+		serverURL = fmt.Sprintf("https://%v/update", *serverAddr)
+	} else {
+		serverURL = fmt.Sprintf("http://%v/update", *serverAddr)
+	}
+	if *pollInterval > *reportInterval {
+		log.Fatalf("Poll interval can not be more that report interval")
+	}
 	runtimeRequiredMetrics := []string{"Alloc", "BuckHashSys", "Frees", "GCCPUFraction", "GCSys", "HeapAlloc",
 		"HeapIdle", "HeapInuse", "HeapObjects", "HeapReleased", "HeapSys", "LastGC", "Lookups",
 		"MCacheInuse", "MCacheSys", "MSpanInuse", "MSpanSys", "Mallocs", "NextGC", "NumForcedGC",
 		"NumGC", "OtherSys", "PauseTotalNs", "StackInuse", "StackSys", "Sys", "TotalAlloc"}
-	pollIntervalDuration := time.Second * time.Duration(pollInterval)
-	reportIntervalDuration := time.Second * time.Duration(reportInterval)
+	pollIntervalDuration := time.Second * time.Duration(*pollInterval)
+	reportIntervalDuration := time.Second * time.Duration(*reportInterval)
 	reportTicker := time.NewTicker(reportIntervalDuration)
 	metricsChan := make(chan []models.Metric, 1)
 	go metrics.GetMetrics(metricsChan, pollIntervalDuration, runtimeRequiredMetrics)
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,15 +1,21 @@
 package main
 
 import (
+	"flag"
 	"github.com/aksenk/go-yandex-metrics/internal/server/handlers"
 	"github.com/aksenk/go-yandex-metrics/internal/server/storage/memstorage"
+	"log"
 	"net/http"
 )
 
-// TODO как запускать тесты сразу по всем директориям?
+// TODO просто вопрос: как запускать тесты сразу по всем директориям?
 func main() {
-	listenAddr := "localhost:8080"
+	listenAddr := flag.String("a", "localhost:8080", "host:port for server listening")
+	flag.Parse()
 	s := memstorage.NewMemStorage()
 	r := handlers.NewRouter(s)
-	http.ListenAndServe(listenAddr, r)
+	log.Printf("Starting web server on %v", *listenAddr)
+	if err := http.ListenAndServe(*listenAddr, r); err != nil {
+		log.Fatalf("Error starting server: %v", err)
+	}
 }
