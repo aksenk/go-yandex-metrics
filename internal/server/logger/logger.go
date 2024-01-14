e6ad3b3827140590ff0fdf764bdf7a0bdd2ebf71
Николай Аксенов <nk_aksenov@betboom.org>
2024-01-14 20:29:59 +0300

    fix logger

--- internal/server/logger/logger.go
+++ internal/server/logger/logger.go
@@ -26,7 +26,7 @@ func (r *loggingResponseWriter) Write(b []byte) (int, error) {
 }
 
 func (r *loggingResponseWriter) WriteHeader(statusCode int) {
-	//r.ResponseWriter.WriteHeader(statusCode)
+	r.ResponseWriter.WriteHeader(statusCode)
 	r.responseData.statusCode = statusCode
 }
 
