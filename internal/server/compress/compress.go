d768fe71c092ad126860fd9c13f8bb3f2935d570
Николай Аксенов <nk_aksenov@betboom.org>
2024-02-02 17:09:39 +0300

    compress only allowed content types

--- internal/server/compress/compress.go
+++ internal/server/compress/compress.go
@@ -10,20 +10,20 @@ import (
 
 type gzipReadCloser struct {
 	io.ReadCloser
-	gr io.Reader
+	gzipReader io.Reader
 }
 
 func (g gzipReadCloser) Read(b []byte) (int, error) {
-	return g.gr.Read(b)
+	return g.gzipReader.Read(b)
 }
 
 type gzipResponseWriter struct {
 	http.ResponseWriter
-	gzw io.Writer
+	gzipWriter io.Writer
 }
 
 func (g gzipResponseWriter) Write(b []byte) (int, error) {
-	return g.gzw.Write(b)
+	return g.gzipWriter.Write(b)
 }
 
 //func NewGzipResponseWriter(w http.ResponseWriter) *gzipResponseWriter {
@@ -40,8 +40,9 @@ func Middleware(next http.Handler) http.Handler {
 			logger.Log.Debugf("Using gzipped request")
 			gr, err := gzip.NewReader(request.Body)
 			if err != nil {
-				// TODO
-				panic(err)
+				logger.Log.Errorf("Can not ungzip incoming reeuest data: %v", err)
+				http.Error(writer, "can not ungzip request data", http.StatusInternalServerError)
+				return
 			}
 			grc := gzipReadCloser{
 				request.Body,
@@ -51,20 +52,32 @@ func Middleware(next http.Handler) http.Handler {
 		}
 
 		if strings.Contains(request.Header.Get("Accept-Encoding"), "gzip") {
-			logger.Log.Debugf("Using gzipped response")
-			// TODO проверять и сжимать только определенные content type
-			// TODO отказаться от gzip.NewWriterLevel() и использовать метод gzip.Reset()
-			gzw, _ := gzip.NewWriterLevel(writer, gzip.BestSpeed)
-			defer gzw.Close()
-			nextResponseWriter = gzipResponseWriter{
-				writer,
-				gzw,
+			allowedGzipContentTypes := []string{"application/json", "text/html"}
+			requestContentType := request.Header.Get("Content-Type")
+			isAllowedContentType := false
+			for _, ct := range allowedGzipContentTypes {
+				if requestContentType == ct {
+					isAllowedContentType = true
+				}
 			}
-			nextResponseWriter.Header().Set("Content-Encoding", "gzip")
+			if isAllowedContentType {
+				logger.Log.Debugf("Using gzipped response")
+				// TODO отказаться от gzip.NewWriterLevel() и использовать метод gzip.Reset()
+				gzw, _ := gzip.NewWriterLevel(writer, gzip.BestSpeed)
+				defer gzw.Close()
+				nextResponseWriter = gzipResponseWriter{
+					writer,
+					gzw,
+				}
+				nextResponseWriter.Header().Set("Content-Encoding", "gzip")
+			} else {
+				logger.Log.Debugf("Header 'Accept-Encoding: gzip' is exist, "+
+					"but this content-type is not allowed for gzipping: %v", requestContentType)
+				nextResponseWriter = writer
+			}
 		} else {
 			nextResponseWriter = writer
 		}
-
 		next.ServeHTTP(nextResponseWriter, request)
 	})
 }
